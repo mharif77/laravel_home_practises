69ca6e4361d34742dcb972cb75477349f7b2170a
MD ARIF HASAN <mdarifhasan077@gmail.com>
2024-10-31 23:58:28 +0600

    controller create

---
+++ practise1/app/Http/Controllers/StudentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+
+
+{
+     function index(){
+            echo "Yes i am here";
+    }
+}
--- practise1/routes/web.php
+++ practise1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [HomeController::class, 'about']);
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/', [StudentController::class, 'index']);
 
 // Route::get('/home', function () {
 //     return view('home');
